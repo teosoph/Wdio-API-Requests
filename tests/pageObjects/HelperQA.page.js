802662084f4d9d8976e86588e038cdbdff7eddf4
teosoph <teosoph@gmail.com>
2022-09-07 14:21:42 +0300

    NonFIXED____request-lib

--- tests/pageObjects/HelperQA.page.js
+++ tests/pageObjects/HelperQA.page.js
@@ -1,38 +1,61 @@
-const axios = require('axios').default;
+// const axios = require('axios').default;
+const request = require('request');
 
 class HelperQA {
-    async configAxios() {
-        return axios.create({
-            baseURL: 'https://mailsac.com/api/',
-            headers: {
-                Host: 'mailsac.com',
-                'Mailsac-Key': `k_wU5aX7qRtV76cAFmtN7Y9006iiyxzAaYZoZ4EAqr4tjfmI51`
-            }
+    //     async configAxios() {
+    //         return axios.create({
+    //             baseURL: 'https://mailsac.com/api/',
+    //             headers: {
+    //                 Host: 'mailsac.com',
+    //                 'Mailsac-Key': `k_wU5aX7qRtV76cAFmtN7Y9006iiyxzAaYZoZ4EAqr4tjfmI51`
+    //             }
+    //         });
+    //     }
+    //     async checkMesages(email) {
+    //         const client = await this.configAxios();
+    //         return client
+    //             .get(`addresses/${email}/messages`)
+    //             .then((response) => {
+    //                 console.log('Response Data ...........................>', response.data);
+    //                 return response.data;
+    //             })
+    //             .catch(this.apiFailureCallback);
+    //     }
+    // catchErrors(error) {
+    //     console.dir(error);
+    //     if (typeof error.response !== 'undefined') {
+    //         console.log('---------------API REQUEST ERROR------------------');
+    //         console.log(error.response.data);
+    //         console.log(error.response.status);
+    //         console.log(error.response.headers);
+    //         console.log('---------------API REQUEST ERROR------------------');
+    //     }
+    //     throw error;
+    // }
+    async getMessageCount(email) {
+        const getMessageCount = {
+            method: 'GET',
+            url: `https://mailsac.com/api/addresses/${email}/message-count`,
+            headers: { 'Mailsac-Key': 'k_wU5aX7qRtV76cAFmtN7Y9006iiyxzAaYZoZ4EAqr4tjfmI51' }
+        };
+        await request(getMessageCount, (error, response, body) => {
+            console.error('!!!!! error:', error);
+            console.log('>>>>>>> statusCode:', response && response.statusCode);
+            console.log('GET message count body ===========================================>', body);
         });
     }
-
-    async checkMesages(email) {
-        const client = await this.configAxios();
-        return client
-            .get(`addresses/${email}/messages`)
-            .then((response) => {
-                console.log('Response Data ...........................>', response.data);
-                return response.data;
-            })
-            .catch(this.apiFailureCallback);
-    }
-
-    catchErrors(error) {
-        console.dir(error);
-        if (typeof error.response !== 'undefined') {
-            console.log('---------------API REQUEST ERROR------------------');
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-            console.log('---------------API REQUEST ERROR------------------');
-        }
-        throw error;
-    }
+    // async getMessageCount(email) {
+    //     let returnData;
+    //     const getMessageCount = {
+    //         method: 'GET',
+    //         url: `https://mailsac.com/api/addresses/${email}/message-count`,
+    //         headers: { 'Mailsac-Key': 'k_wU5aX7qRtV76cAFmtN7Y9006iiyxzAaYZoZ4EAqr4tjfmI51' }
+    //     };
+    //     await request(getMessageCount, (error, response, body)).then((response) => {
+    //         returnData = response.data;
+    //     });
+    //     return returnData;
+    // }
 }
 
 module.exports = new HelperQA();
--- tests/specs/checkEmail.spec.js
+++ tests/specs/checkEmail.spec.js
@@ -5,33 +5,34 @@ const email = `teosoph@mailsac.com`;
 
 describe('api request from wdio ', () => {
     it('check Email', async () => {
-        await HelperQA.checkMesages(email);
+        // await HelperQA.checkMesages(email);
+        await HelperQA.getMessageCount(email);
     });
 });
 
-const getMessageCount = {
-    method: 'GET',
-    url: `https://mailsac.com/api/addresses/${email}/message-count`,
-    headers: { 'Mailsac-Key': 'k_wU5aX7qRtV76cAFmtN7Y9006iiyxzAaYZoZ4EAqr4tjfmI51' }
-};
-request(getMessageCount, (error, response, body) => {
-    console.error('!!!!! error:', error);
-    console.log('>>>>>>> statusCode:', response && response.statusCode);
-    console.log('GET message count body ---------------------------------------->', body);
-});
+// const getMessageCount = {
+//     method: 'GET',
+//     url: `https://mailsac.com/api/addresses/${email}/message-count`,
+//     headers: { 'Mailsac-Key': 'k_wU5aX7qRtV76cAFmtN7Y9006iiyxzAaYZoZ4EAqr4tjfmI51' }
+// };
+// request(getMessageCount, (error, response, body) => {
+//     console.error('!!!!! error:', error);
+//     console.log('>>>>>>> statusCode:', response && response.statusCode);
+//     console.log('GET message count body ===========================================>', body);
+// });
 
-const postValidationsAddresses = {
-    method: 'POST',
-    url: 'https://mailsac.com/api/validations/addresses',
-    headers: {
-        'content-type': 'application/json',
-        'Mailsac-Key': 'k_wU5aX7qRtV76cAFmtN7Y9006iiyxzAaYZoZ4EAqr4tjfmI51'
-    },
-    body: { emails: ['teosoph@mailsac.com'] },
-    json: true
-};
-request(postValidationsAddresses, (error, response, body) => {
-    console.error('!!!!! error:', error);
-    console.log('>>>>>>> statusCode:', response && response.statusCode);
-    console.log('POST validations addresses body ---------------------------------------->', body);
-});
+// const postValidationsAddresses = {
+//     method: 'POST',
+//     url: 'https://mailsac.com/api/validations/addresses',
+//     headers: {
+//         'content-type': 'application/json',
+//         'Mailsac-Key': 'k_wU5aX7qRtV76cAFmtN7Y9006iiyxzAaYZoZ4EAqr4tjfmI51'
+//     },
+//     body: { emails: ['teosoph@mailsac.com'] },
+//     json: true
+// };
+// request(postValidationsAddresses, (error, response, body) => {
+//     console.error('!!!!! error:', error);
+//     console.log('>>>>>>> statusCode:', response && response.statusCode);
+//     console.log('POST validations addresses body ---------------------------------------->', body);
+// });
